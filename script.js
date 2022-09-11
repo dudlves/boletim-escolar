9a5f521c48ac7e83f84309b45b911f465825dd87
dudlves <dudlves17@gmail.com>
2022-09-10 23:41:35 -0300

    Tentando fazer o boletim

---
+++ script.js
@@ -0,0 +1,99 @@
+// const primeiroBimestre = 20;
+// const segundoBimestre = 20;
+// const terceiroBimestre = 20;
+// const quartoBimestre = 20;
+// const valorTotal = primeiroBimestre + segundoBimestre + terceiroBimestre + quartoBimestre
+
+
+
+
+// const matematica = ;
+// const portugues =18;
+// const fisica = 12;
+// const quimica = 20;
+// const historia = 20;
+// const geografia = 20;
+
+const primeiroBimestre = {
+    matematica : 20,
+    portugues :18,
+    fisica :12,
+    quimica : 20,
+    historia :20,
+    geografia : 0
+}
+
+const segundoBimestre = {
+    matematica : 20,
+    portugues :18,
+    fisica :12,
+    quimica : 20,
+    historia :20,
+    geografia : 10
+}
+
+const terceiroBimestre = {
+    matematica : 20,
+    portugues :18,
+    fisica :12,
+    quimica : 20,
+    historia :20,
+    geografia : 10
+}
+
+const quartoBimestre = {
+    matematica : 20,
+    portugues :18,
+    fisica :12,
+    quimica : 20,
+    historia :20,
+    geografia : 3
+}
+
+const matematica = (primeiroBimestre.matematica + segundoBimestre.matematica + terceiroBimestre.matematica + quartoBimestre.matematica) 
+const portugues = (primeiroBimestre.portugues + segundoBimestre.portugues + terceiroBimestre.portugues + quartoBimestre.portugues) 
+const fisica = (primeiroBimestre.fisica + segundoBimestre.fisica + terceiroBimestre.fisica + quartoBimestre.fisica) 
+const quimica = (primeiroBimestre.quimica + segundoBimestre.quimica + terceiroBimestre.quimica + quartoBimestre.quimica)
+const historia = (primeiroBimestre.historia + segundoBimestre.historia +terceiroBimestre.historia + quartoBimestre.historia)
+const geografia= (primeiroBimestre.geografia + segundoBimestre.geografia +terceiroBimestre.geografia + quartoBimestre.geografia)
+
+if(matematica >= 60){
+    console.log("Você passou!")
+}else{
+    console.log("Você não passou!")
+}
+ 
+if(portugues >= 60){
+    console.log("Você passou!")
+}else{
+    console.log("Você não passou!")
+}
+ 
+if(fisica >= 60){
+    console.log("Você passou!")
+}else{
+    console.log("Você não passou!")
+}
+ 
+if(quimica >= 60){
+    console.log("Você passou!")
+}else{
+    console.log("Você não passou!")
+}
+
+if(historia >= 60){
+    console.log("Você passou!")
+}else{
+    console.log("Você não passou!")
+}
+ 
+if(geografia >= 60){
+    console.log("Você passou!")
+}else{
+    console.log("Você não passou!")
+}
+ 
+
+ 
+
+
